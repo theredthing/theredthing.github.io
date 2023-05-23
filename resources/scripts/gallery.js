e76f1e47bde035481903550a6f263404c8133a53
Morar Andrei - Alexandru <whiteex@icloud.com>
2023-05-23 03:01:24 +0300

    May 23rd, 2023
    - Upload changes from remote repo

---
+++ resources/scripts/gallery.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.container .picture img').forEach(picture =>{
+    picture.onclick = () => {
+        document.querySelector('.popup-pic').style.display = 'block';
+        document.querySelector('.popup-pic img').src = picture.getAttribute('src');
+    }
+})
+
+document.querySelector('.popup-pic span').onclick = () => {
+    document.querySelector('.popup-pic').style.display = 'none';
+}
---
+++ resources/scripts/redirect.js
@@ -0,0 +1,16 @@
+// var pixelart = document.querySelector('.gallery-photos .picture.pixelart')
+var digital = document.querySelector('.gallery-photos .picture.digital')
+var traditional = document.querySelector('.gallery-photos .picture.traditional')
+
+// pixelart.onclick = () => {
+//     window.open("../../gallery/pixelart.html")
+//     console.log("Pixel")
+// }
+
+digital.onclick = () => {
+    window.open("../../gallery/digital.html")
+}
+
+traditional.onclick = () => {
+    window.open("../../gallery/traditional.html")
+}
